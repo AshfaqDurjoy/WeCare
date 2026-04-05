2d13ff97a7c3e43f83c217f2df06e14a8d9e7f14
Anupoma Haque Anika <143196671+Anupoma-Haque@users.noreply.github.com>
2026-04-06 01:08:35 +0600

    Update

--- WeCare/Controllers/HomeController.cs
+++ WeCare/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿/*using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WeCare.Data;
@@ -154,4 +154,4 @@ public class HomeController : Controller
     {
         return View();
     }
-}*/
+}
