8b21f15e342de8ea9f8fb698099b729ae88b2010
Anupoma Haque Anika <143196671+Anupoma-Haque@users.noreply.github.com>
2026-04-06 01:17:37 +0600

    Dashboard Working

---
+++ WeCare/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,16 @@
+﻿using WeCare.Models;
+
+namespace WeCare.ViewModels;
+
+public class DashboardViewModel
+{
+    public string UserTypeName { get; set; } = string.Empty;
+    public string UserName { get; set; } = string.Empty;
+    public Donor? DonorProfile { get; set; }
+    public Seeker? SeekerProfile { get; set; }
+    public Hospital? HospitalProfile { get; set; }
+    public WeCareOrganization? OrganizationProfile { get; set; }
+    public DateTime? LastDonationDate { get; set; }
+    public List<Campaign> UpcomingCampaigns { get; set; } = new();
+    public List<EventDisplayMV> ParticipatedEvents { get; set; } = new();
+}
