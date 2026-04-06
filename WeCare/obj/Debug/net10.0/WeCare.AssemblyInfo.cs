fb40b39eb4a91e32b756e6a7eb3fc1a664145012
Zarin Tasnim Fema <zarintasnimfema@gmail.com>
2026-04-06 11:37:14 +0600

    Organization

--- WeCare/obj/Debug/net10.0/WeCare.AssemblyInfo.cs
+++ WeCare/obj/Debug/net10.0/WeCare.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeCare")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+138b96e42c92a9bfb8d823364751ac529fdf7eb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd9e1b06c6b28fdfc03ba5a85c9775ad048b8899")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeCare")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeCare")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
