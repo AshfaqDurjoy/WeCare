e7891b63afbaf9e3989babf1918238a9604496e2
Zarin Tasnim Fema <zarintasnimfema@gmail.com>
2026-04-06 11:15:37 +0600

    Individual registration

--- WeCare/obj/Debug/net10.0/WeCare.AssemblyInfo.cs
+++ WeCare/obj/Debug/net10.0/WeCare.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeCare")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb4e5aa24a3c2995104827cd509ea7ff31732043")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f57e93d55c81ed989c8fe634a4c874cac07a2d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeCare")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeCare")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
