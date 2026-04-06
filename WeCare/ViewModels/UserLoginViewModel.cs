725651117dd1fed7bc2de06a4e110a757aa1e03a
Anupoma Haque Anika <143196671+Anupoma-Haque@users.noreply.github.com>
2026-04-06 10:09:29 +0600

    User Model working properly

---
+++ WeCare/ViewModels/UserLoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WeCare.ViewModels;
+
+public class UserLoginViewModel
+{
+    [Required, EmailAddress]
+    public string EmailAddress { get; set; } = string.Empty;
+
+    [Required, DataType(DataType.Password)]
+    public string Password { get; set; } = string.Empty;
+
+    public bool RememberMe { get; set; }
+}
